04afa0443abfa26031ae11acf30a9ac658718ebe
Jmarsaj <jerinsajimon@gmail.com>
2023-08-10 11:50:30 -0700

    implemented the first 4 bonus features

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -1,69 +1,84 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts "@@ #{message}"
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def float?(num)
+  num.to_f.to_s == num
 end
 
-def operator_to_word(operator)
-  case operator
-  when '1' then 'Adding'
-  when '2' then 'Subtracting'
-  when '3' then 'Multiplying'
-  when '4' then 'Dividing'
+def integer?(num)
+  # num.to_i != 0 || num == '0'
+  num.to_i.to_s == num
+end
+
+def valid_number?(num)
+  # integer?(num) || float?(num)
+  if integer?(num)
+    return num.to_i
+  elsif float?(num)
+    return num.to_f
+  else
+    return false
   end
 end
 
-prompt("Welcome to Calculator! Enter name")
+def operator_to_message(operator)
+  message = case operator
+            when '1' then 'Adding'
+            when '2' then 'Subtracting'
+            when '3' then 'Multiplying'
+            when '4' then 'Dividing'
+            end
+
+  message
+end
+
+prompt(MESSAGES['welcome'])
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Not a valid name")
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
 end
 
-prompt("Hi #{name}!")
+prompt(MESSAGES['greeting'] + name + "!")
 
 loop do
   number1 = ''
   loop do
-    prompt("Enter first number")
+    prompt(MESSAGES['first_number'])
     number1 = gets.chomp
 
     if valid_number?(number1)
+      number1 = valid_number?(number1)
       break
     else
-      prompt("Not a vaild number")
+      prompt(MESSAGES['valid_number'])
     end
   end
 
   number2 = ''
   loop do
-    prompt("Enter second number")
+    prompt(MESSAGES['second_number'])
     number2 = gets.chomp
 
     if valid_number?(number2)
+      number2 = valid_number?(number2)
       break
     else
-      prompt("Not a vaild number")
+      prompt(MESSAGES['valid_number'])
     end
   end
 
-  operator_prompt = <<-MSG
-  Which operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  prompt(operator_prompt)
+  prompt(MESSAGES['operator_prompt'])
   operator = ''
 
   loop do
@@ -72,26 +87,26 @@ loop do
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Enter 1, 2, 3 or 4")
+      prompt(MESSAGES['valid_operator'])
     end
   end
 
-  prompt("#{operator_to_word(operator)} the two numbers...")
+  prompt(operator_to_message(operator) + MESSAGES['operation_message'])
 
   result = case operator
            when '1'
-             number1.to_i + number2.to_i
+             number1 + number2
            when '2'
-             number1.to_i - number2.to_i
+             number1 - number2
            when '3'
-             number1.to_i * number2.to_i
+             number1 * number2
            when '4'
              number1.to_f / number2.to_f
            end
 
-  prompt("The result is #{result}")
-  prompt("Enter Y to perform another calculation")
+  prompt(MESSAGES['result'] + "#{result.round(3)}")
+  prompt(MESSAGES['another_one'])
   answer = gets.chomp
   break unless answer.downcase().start_with?('y')
 end
-prompt("GOODBYE \n")
+prompt(MESSAGES['end'])
