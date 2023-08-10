b62f7ea7a77ff054ceaf8c8bf9e71ab4beab6d2b
Jmarsaj <jerinsajimon@gmail.com>
2023-08-10 12:41:13 -0700

    syntax edit

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -96,14 +96,10 @@ loop do
   prompt('operation_message', operator_to_message(operator))
 
   result = case operator
-           when '1'
-             number1 + number2
-           when '2'
-             number1 - number2
-           when '3'
-             number1 * number2
-           when '4'
-             number1.to_f / number2.to_f
+           when '1' then number1 + number2
+           when '2' then number1 - number2
+           when '3' then number1 * number2
+           when '4' then number1.to_f / number2.to_f
            end
 
   prompt('result', "#{result.round(3)}")
